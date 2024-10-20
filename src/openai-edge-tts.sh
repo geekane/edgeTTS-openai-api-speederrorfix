cbc49af88d04d31a5fbe96ee345d87b5321961d1
orztv <qi@outlook.cz>
2024-10-20 22:18:17 +0800

    update

--- src/openai-edge-tts.sh
+++ src/openai-edge-tts.sh
@@ -16,4 +16,7 @@ pip install -r requirements.txt
 # .env 文件
 cp .env.example .env
 
+# 替换 .env 文件中的 API_KEY
+sed -i "s|API_KEY=your_api_key_here|API_KEY=$(cat /run/secrets/apikey)|g" $HOMEDIR/edgeTTS-openai-api/src/api/.env
+
 echo "openai-edge-tts 安装完成"
